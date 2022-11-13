def69391b21ef1861d54c05cdd5bcb0b93a4cf79
KongDuHwan <kong_du@naver.com>
2022-11-14 03:27:04 +0900

    예약날짜 클릭 이벤트
    예약 날짜 클릭 시 캘린더뷰 출력 및 닫기
    캘린더 뷰 클릭 시 월, 일 업데이트

--- ParkingLot/app/src/main/java/softwareProject/parkingLot/User/ReservationActivity.kt
+++ ParkingLot/app/src/main/java/softwareProject/parkingLot/User/ReservationActivity.kt
@@ -1,29 +1,107 @@
 package softwareProject.parkingLot.User
 
 import android.graphics.Typeface
+import android.os.Build
 import android.os.Bundle
-import android.text.SpannableStringBuilder
-import android.text.Spanned
-import android.text.style.StyleSpan
+import android.view.View
+import android.widget.CalendarView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import softwareProject.parkingLot.R
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class ReservationActivity : AppCompatActivity() {
 
     lateinit var selectDate1: TextView
-    var dateString = ""
-    var timeString = ""
+    lateinit var selectDate2: TextView
+    lateinit var calView1: CalendarView
+    lateinit var calView2: CalendarView
 
+    var selectYear1: Int = 0
+    var selectMonth1: Int = 0
+    var selectDay1: Int = 0
+    var selectYear2: Int = 0
+    var selectMonth2: Int = 0
+    var selectDay2: Int = 0
+
+    var cal1_already_ON = false
+    var cal2_already_ON = false
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservation)
 
-        selectDate1 = findViewById(R.id.selectDate1)
-        selectDate1.setOnClickListener {
-            val textData: String = selectDate1.text.toString()
-            val builder = SpannableStringBuilder(textData)
-            val boldSpan = StyleSpan(Typeface.BOLD)
+        setViewId()
+        calViewInit()
+
+        selectDate1.setOnClickListener(View.OnClickListener {
+            calView2.visibility = View.GONE
+            cal2_already_ON = false
+
+            selectDate2.setTypeface(null, Typeface.NORMAL)
+            selectDate2.setBackgroundResource(R.drawable.button_click_off)
+
+            if (cal1_already_ON) {
+                selectDate1.setTypeface(null, Typeface.NORMAL)
+                selectDate1.setBackgroundResource(R.drawable.button_click_off)
+                calView1.visibility = View.GONE
+                cal1_already_ON = false
+            } else {
+                selectDate1.setTypeface(null, Typeface.BOLD)
+                selectDate1.setBackgroundResource(R.drawable.button_click_on)
+                calView1.visibility = View.VISIBLE
+                cal1_already_ON = true
+            }
+        })
+        selectDate2.setOnClickListener(View.OnClickListener {
+            calView1.visibility = View.GONE
+            cal1_already_ON = false
+            selectDate1.setTypeface(null, Typeface.NORMAL)
+            selectDate1.setBackgroundResource(R.drawable.button_click_off)
+
+
+            if (cal2_already_ON) {
+                selectDate2.setTypeface(null, Typeface.NORMAL)
+                selectDate2.setBackgroundResource(R.drawable.button_click_off)
+                calView2.visibility = View.GONE
+                cal2_already_ON = false
+            } else {
+                selectDate2.setTypeface(null, Typeface.BOLD)
+                selectDate2.setBackgroundResource(R.drawable.button_click_on)
+                calView2.visibility = View.VISIBLE
+                cal2_already_ON = true
+            }
+        })
+
+        calView1.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            selectDate1.setText("${month}월 ${dayOfMonth}일 (월)")
+        }
+        calView2.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            selectDate2.setText("${month}월 ${dayOfMonth}일 (월)")
         }
     }
+    fun setViewId(){
+        selectDate1 = findViewById(R.id.selectDate1)
+        selectDate2 = findViewById(R.id.selectDate2)
+        calView1 = findViewById<CalendarView>(R.id.calView1)
+        calView2 = findViewById<CalendarView>(R.id.calView2)
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun calViewInit(){
+        calView1.visibility = View.GONE
+        calView2.visibility = View.GONE
+
+        var current=LocalDateTime.now()
+        var formatter=DateTimeFormatter.ofPattern("MM월 dd일 (E)").withLocale(Locale.forLanguageTag("ko"))
+        var formatted=current.format(formatter)
+        selectDate1.setText(formatted)
+        selectDate2.setText(formatted)
+    }
+
+
 }
